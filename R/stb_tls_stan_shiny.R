493fab6eb7bc22c6e9faa3b04e1eac972d056a67
CG Wang <chenguang.wang@regeneron.com>
2024-06-03 07:50:56 -0400

    initial commit

---
+++ R/stb_tls_stan_shiny.R
@@ -0,0 +1,96 @@
+## -----------------------------------------------------------------------------
+##
+##                 STAN
+##
+## -----------------------------------------------------------------------------
+
+#' Call STAN models
+#'
+#' Call STAN models. Called by \code{psrwe_powerp}.
+#'
+#' @param lst_data List of study data to be passed to STAN
+#' @param stan_mdl STAN model name
+#' @param chains STAN parameter. Number of Markov chainsm
+#' @param iter STAN parameter. Number of iterations
+#' @param warmup STAN parameter. Number of burnin.
+#' @param control STAN parameter. See \code{rstan::stan} for details.
+#' @param ... other options to call STAN sampling such as \code{thin},
+#'     \code{algorithm}. See \code{rstan::sampling} for details.#'
+#'
+#' @return Result from STAN sampling
+#'
+#' @export
+#'
+stb_stan <- function(lst_data,
+                     stan_mdl = c("nb_mix", "nb_pp"),
+                     chains = 4, iter = 2000, warmup = 1000, cores = 4,
+                     control = list(adapt_delta = 0.95), ...) {
+
+    stan_mdl <- match.arg(stan_mdl)
+    stan_rst <- rstan::sampling(stanmodels[[stan_mdl]],
+                                data    = lst_data,
+                                chains  = chains,
+                                iter    = iter,
+                                warmup  = warmup,
+                                cores   = cores,
+                                control = control,
+                                ...)
+
+    stan_rst
+}
+
+
+## -----------------------------------------------------------------------------
+##
+##                 SHINY
+##
+## -----------------------------------------------------------------------------
+
+#' Run Web-Based application
+#'
+#' Call Shiny to run \code{statidea} as a web-based application.
+#'
+#' @details
+#'
+#' A web browser will be brought up for users to access the GUI
+#'
+#'
+#' @export
+#'
+stb_shiny <- function(appname = "bayes2", pkgname = "simutb") {
+
+    req_pkgs        <- c("shiny", "shinythemes", "DT",
+                         "knitr", "rmarkdown", "pander")
+
+    chk_uninstalled <- sapply(req_pkgs,
+                              function(x) {
+                                  !requireNamespace(x,
+                                                    quietly = TRUE)
+                              })
+
+    chk_inx         <- which(chk_uninstalled)
+
+    if (0 < length(chk_inx)) {
+
+        msg <- paste("For the Shiny app to work, please install ",
+                     ifelse(1 < length(chk_inx), "packages ", "package "),
+                     paste(req_pkgs[chk_inx], collapse = ", "),
+                     " by \n install.packages(",
+                     paste(paste("'",
+                                 req_pkgs[chk_inx],
+                                 "'",
+                                 sep = ""), collapse = ", "),
+                     ") \n  ",
+                     sep = "")
+
+        stop(msg, call. = FALSE)
+    }
+
+    app_dir <- system.file(appname, package = "simutb")
+    if (app_dir == "") {
+        stop("Could not find Shiny directory. Try re-installing `simutb`.",
+             call. = FALSE)
+    }
+
+    shiny::runApp(app_dir, display.mode = "normal")
+}
---
+++ R/zzz_package.R
@@ -0,0 +1,23 @@
+#' The 'simubayes' package.
+#'
+#' @docType package
+#' @name    simubayes-package
+#' @aliases simubayes
+#' @useDynLib simubayes, .registration = TRUE
+#'
+#' @import methods
+#' @import stats
+#' @import ggplot2
+#'
+#' @importFrom grDevices colors
+#' @importFrom graphics axis box legend lines par plot points text arrows grid
+#'     rect
+#' @importFrom parallel detectCores
+#' @importFrom utils as.roman
+#' @importFrom dplyr %>% group_by_ group_by summarize mutate count mutate_if
+#'     rename filter select arrange ungroup n distinct left_join if_else rowwise
+#' @importFrom tidyr gather
+#' @importFrom data.table rbindlist
+#'
+#'
+NULL
